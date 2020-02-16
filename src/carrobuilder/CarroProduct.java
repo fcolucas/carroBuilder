8a2d43f9b7be185208084cb9fac551b98f55e4a0
Francisco Lucas <fcolucaslima14@gmail.com>
2020-02-15 22:04:33 -0300

    Criando produto

---
+++ src/carrobuilder/CarroProduct.java
@@ -0,0 +1,11 @@
+package carrobuilder;
+
+public class CarroProduct {
+	//Armazenando as informações de um carro
+	
+	double preco;
+	String dscMotor;
+	int anoFabricacao;
+	String modelo;
+	String montadora;
+}
